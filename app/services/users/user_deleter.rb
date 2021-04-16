43206ee83b9f2cee5ec6159765a1832af2cb182b
yogesh mongar <yogesh.mongar@selise.ch>
2021-04-16 13:41:49 +0600

    reset email for deleted users

--- app/services/users/user_deleter.rb
+++ app/services/users/user_deleter.rb
@@ -4,8 +4,18 @@ module Users
   class UserDeleter < BaseService
     include UserFinder
 
+    TIME_FORMAT = '%Y_%m_%d_%H_%M'
+
+    # after soft delete reset email so that next time the same user is added
+    # you don't get email uniqueness error.
+    # also prepending current time to maintain uniqueness of deleted records with same email.
+    #
     def call
-      user.discard!
+      user.discard!.tap do
+        user.update_column(
+          :email, user.email.prepend(Time.current.strftime("#{TIME_FORMAT}_"))
+        )
+      end
     end
   end
 end
--- spec/graphql/mutations/delete_user_spec.rb
+++ spec/graphql/mutations/delete_user_spec.rb
@@ -7,13 +7,35 @@ RSpec.describe Mutations::DeleteUser do
   let_it_be(:team_standard) { create(:user, :team_standard) }
 
   describe '.resolve' do
-    it 'deletes the user' do
-      response, errors = formatted_response(query(id: team_standard.id), current_user: team_expert, key: :deleteUser)
-      expect(errors).to be_nil
-      expect(response.status).to be(true)
+    context 'for a valid user' do
+      it 'soft deletes the user' do
+        response, errors = formatted_response(query(id: team_standard.id), current_user: team_expert, key: :deleteUser)
+        expect(errors).to be_nil
+        expect(response.status).to be(true)
 
-      expect { User.find(team_standard.id) }.to raise_error(ActiveRecord::RecordNotFound)
-      expect { User.unscoped.find(team_standard.id) }.not_to raise_error
+        expect { User.find(team_standard.id) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { User.unscoped.find(team_standard.id) }.not_to raise_error
+        expect(team_standard.reload.discarded_at).to be_present
+      end
+
+      it 'resets user email with current time prefixed' do
+        response, errors = formatted_response(query(id: team_standard.id), current_user: team_expert, key: :deleteUser)
+        expect(errors).to be_nil
+        expect(response.status).to be(true)
+
+        expect(team_standard.reload.email).to include(Time.current.strftime('%Y_%m_%d_%H_'))
+      end
+    end
+
+    context 'when user does not exist in the portal' do
+      it 'responds with error' do
+        response, errors = formatted_response(
+          query(id: '16c85b18-473d-4f5d-9ab4-666c7faceb6c\"'), current_user: team_expert, key: :deleteUser
+        )
+
+        expect(response.user).to be_nil
+        expect(errors[0]).to include("Couldn't find Telco::Uam::User with 'id'=16c85b18-473d-4f5d-9ab4-666c7faceb6c\"")
+      end
     end
   end
 
