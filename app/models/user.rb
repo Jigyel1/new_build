6b3553f2539f124e83951c41bc5bd1420f29152a
yogesh mongar <yogesh.mongar@selise.ch>
2021-04-26 22:56:43 +0600

    comment on disabling strict loading by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ User = Telco::Uam::User
 User.class_eval do
   include Discard::Model
 
-  self.strict_loading_by_default = false # TODO: remove this!
+  # Since user is derived from the engine(telco-uam), it has no knowledge
+  # of it's associations. To support `strict_loading_by_default` we have to add custom
+  # preloading of associations, thereby making the code a bit more messy,
+  # which I feel is not worth the slight benefit achieved.
+  self.strict_loading_by_default = false
 
   belongs_to :role, inverse_of: :users
-  has_one :profile, inverse_of: :user, strict_loading: false, dependent: :destroy
+  has_one :profile, inverse_of: :user, dependent: :destroy
   has_one :address, as: :addressable, inverse_of: :addressable, dependent: :destroy
 
   validates :profile, presence: true
