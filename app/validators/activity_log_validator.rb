41235a9b364a7578642a4969351cf19bbc6b3b1d
yogesh mongar <yogesh.mongar@selise.ch>
2021-05-07 13:51:02 +0600

    activity error catch fix

--- app/validators/activity_log_validator.rb
+++ app/validators/activity_log_validator.rb
@@ -2,10 +2,10 @@
 
 class ActivityLogValidator < ActiveModel::EachValidator
   def validate_each(record, _attribute, value)
-    case record.verb.to_sym
-    when :status_updated
+    case record.verb
+    when 'status_updated'
       !value.dig('parameters', 'active').nil? || record.errors.add(:log_data, :active_flag_blank)
-    when :role_updated
+    when 'role_updated'
       value.dig('parameters', 'role').present? || record.errors.add(:log_data, :role_blank)
     end
   end
--- lib/activity_populator.rb
+++ lib/activity_populator.rb
@@ -21,7 +21,7 @@ class ActivityPopulator
         recipient_email: recipient.email,
         parameters: parameters
       }
-    ).then { |activity| activity || (puts activity.errors.full_messages) } # rubocop:disable Rails/Output
+    ).then { |activity| activity.persisted? ? activity : (puts activity.errors.full_messages) } # rubocop:disable Rails/Output
   end
 end
 # Rollbar.error(activity.errors.full_messages)
