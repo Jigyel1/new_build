01e1d2a864b49826abe722ca683c391cb8908f75
yogesh mongar <yogesh.mongar@selise.ch>
2021-05-17 12:31:49 +0600

    azure key updates

--- config/initializers/cnc_storage.rb
+++ config/initializers/cnc_storage.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Cnc::Storage.configure do |config|
-  config.bucket = 'stg-assets'
+  config.bucket = Rails.application.credentials.dig(:azure_storage, :container)
   config.endpoint = 'https://fra1.digitaloceanspaces.com'
-  config.secret_access_key = Rails.application.credentials.azure_storage[:storage_account_name]
-  config.access_key_id = Rails.application.credentials.azure_storage[:storage_access_key]
+  config.secret_access_key = Rails.application.credentials.dig(:azure_storage, :storage_account_name)
+  config.access_key_id = Rails.application.credentials.dig(:azure_storage, :storage_access_key)
   config.region = 'fra1'
   config.cdn_url = 'https://assets.selise.tech'
 end
