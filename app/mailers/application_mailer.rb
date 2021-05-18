28450194539a7e1f56ed1864069ebed946fa3bcf
yogesh mongar <yogesh.mongar@selise.ch>
2021-05-18 16:18:01 +0600

    inline attachment - upc logo in the invitation email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  before_action :inline_logo
+
+  def inline_logo
+    attachments.inline['logo.png'] = File.read(Rails.root.join('app/assets/images/upc-logo.png'))
+  end
 end
