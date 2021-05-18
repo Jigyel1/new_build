3a4305c93ac90399384e6aa04da59f84376f4aaf
yogesh mongar <yogesh.mongar@selise.ch>
2021-05-18 14:01:59 +0600

    remove redirect on token verification failure

--- lib/devise/custom_failure_app.rb
+++ lib/devise/custom_failure_app.rb
@@ -2,13 +2,13 @@
 
 module Devise
   class CustomFailureApp < ::Devise::FailureApp
+    # We need to be able to differentiate if the failure is because of the invalid
+    # token issue, or if it is because user can login through azure, but hasn't been
+    # invited to the portal. For the former we return a status code 401 and 403 for the latter.
     def respond
-      super
-
-      # We need to be able to differentiate if the failure is because of the invalid
-      # token issue, or if it is because user can login through azure, but hasn't been
-      # invited to the portal. For the former we return a status code 401 and 403 for the latter.
-      self.status = 403 if i18n_message == I18n.t('devise.sign_in.not_found')
+      self.status = i18n_message == I18n.t('devise.sign_in.not_found') ? 403 : 401
+      self.content_type = 'json'
+      self.response_body = { errors: [i18n_message] }.to_json
     end
   end
 end
