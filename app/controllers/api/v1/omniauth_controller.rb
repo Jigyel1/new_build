03cfbba3bee6bd13ad1d2a23d0645f64ca56c48c
yogesh mongar <yogesh.mongar@selise.ch>
2021-05-24 17:04:54 +0600

    microsoft endpoint

--- app/controllers/api/v1/omniauth_controller.rb
+++ app/controllers/api/v1/omniauth_controller.rb
@@ -2,13 +2,13 @@ module Api
   module V1
     class OmniauthController < ActionController::API
       def authorization_endpoint
-        render json: { endpoint: "#{endpoint}?#{URI.encode_www_form(params)}" }, status: :unauthorized
+        render json: { endpoint: "#{endpoint}?#{URI.encode_www_form(params)}" }, status: :ok
       end
 
       private
 
       def endpoint
-        api_v1_user_azure_activedirectory_v2_omniauth_authorize_path
+        'https://login.microsoftonline.com/5c6dd6a7-f0c7-4a32-8f7c-9ca7cebf6e87/oauth2/v2.0/authorize'
       end
 
       def params
