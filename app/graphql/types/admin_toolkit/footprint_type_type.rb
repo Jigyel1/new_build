3d2d34b55ff5b7c2b461fbb3abe8f7e20dee2d5a
yogesh mongar <yogesh.mongar@selise.ch>
2021-08-20 11:40:48 +0600

    footprint provider send value

--- app/graphql/types/admin_toolkit/footprint_type_type.rb
+++ app/graphql/types/admin_toolkit/footprint_type_type.rb
@@ -6,6 +6,10 @@ module Types
       field :id, ID, null: true
       field :index, Int, null: true
       field :provider, String, null: true
+
+      def provider
+        ::AdminToolkit::FootprintType.providers[object.provider]
+      end
     end
   end
 end
