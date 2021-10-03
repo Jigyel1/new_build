05775c5c79f11de863c7a4aa75a90fc0374c843c
yogesh mongar <yogesh.mongar@selise.ch>
2021-10-03 10:27:37 +0600

    code comments, cleanups etc

--- db/migrate/20210911120552_add_new_fields_to_projects.rb
+++ db/migrate/20210911120552_add_new_fields_to_projects.rb
@@ -1,23 +1,11 @@
 # frozen_string_literal: true
 
 class AddNewFieldsToProjects < ActiveRecord::Migration[6.1]
-  def change # rubocop:disable Metrics/SeliseMethodLength
+  def change
     safety_assured do
-      add_reference(
-        :projects,
-        :competition,
-        foreign_key: { to_table: :admin_toolkit_competitions },
-        type: :uuid
-      )
-
-      add_reference(
-        :projects,
-        :incharge,
-        foreign_key: { to_table: :telco_uam_users },
-        type: :uuid
-      )
-
       change_table :projects, bulk: true do |t|
+        t.references :competition, foreign_key: { to_table: :admin_toolkit_competitions }, type: :uuid
+        t.references :incharge, foreign_key: { to_table: :telco_uam_users }, type: :uuid
         t.text :analysis
         t.boolean :customer_request
         t.jsonb :verdicts, default: {}
--- db/views/users_lists_v01.sql
+++ db/views/users_lists_v01.sql
@@ -1,13 +1,13 @@
-SELECT telco_uam_users.id  AS id,
-       telco_uam_users.active AS active,
-       telco_uam_users.email AS email,
-       CONCAT(profiles.firstname, ' ', profiles.lastname) AS name,
-       profiles.phone      AS phone,
-       roles.name AS role
+SELECT telco_uam_users.id                                   AS id,
+       telco_uam_users.active                               AS active,
+       telco_uam_users.email                                AS email,
+       CONCAT(profiles.firstname, ' ', profiles.lastname)   AS name,
+       profiles.phone                                       AS phone,
+       roles.name                                           AS role
 
 FROM telco_uam_users
-         JOIN profiles ON profiles.user_id = telco_uam_users.id
-        JOIN roles ON roles.id = telco_uam_users.role_id
+    JOIN profiles ON profiles.user_id = telco_uam_users.id
+    JOIN roles ON roles.id = telco_uam_users.role_id
 
 WHERE telco_uam_users.discarded_at IS NULL
 
