48c52d85abf07364f8b9fc3f73eb60b76137c7aa
yogesh mongar <yogesh.mongar@selise.ch>
2021-10-04 12:51:31 +0600

    project statuses hash in project list as underscored keys instead of direct db values

--- app/graphql/types/project_connection_type.rb
+++ app/graphql/types/project_connection_type.rb
@@ -7,7 +7,7 @@ module Types
     field :count_by_statuses, GraphQL::Types::JSON, null: true
 
     def count_by_statuses
-      ProjectsList.select(:status).group(:status).count
+      ProjectsList.select(:status).group(:status).count.transform_keys { |key| key.downcase.gsub(' ', '_') }
     end
   end
 end
