c3269a78500ba3ace47da6515d91b256502a13b9
yogesh mongar <yogesh.mongar@selise.ch>
2021-10-20 17:38:34 +0600

    correct sfn entry in the seeds

--- db/seeds/competitions.rb
+++ db/seeds/competitions.rb
@@ -5,7 +5,7 @@ HEADERS = %w[sfn factor lease_rate description].freeze
 [
   [false, 'FTTH Swisscom', 1.5, 75, 'The best situation'],
   [true, 'FTTH local provider', 1.7, 75, 'The best situation - reloaded'],
-  [true, 'Swisscom DSL', 1.9, 75, 'The best situation - ultimatum']
+  [false, 'Swisscom DSL', 1.9, 75, 'The best situation - ultimatum']
 ].each do |arr|
   name = arr.shift
   create_record(HEADERS.zip(arr).to_h) { AdminToolkit::Competition.find_or_initialize_by(name: name) }
