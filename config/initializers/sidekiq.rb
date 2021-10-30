1cc69705c576cda26dd4fe1c17e4d243b3f31794
yogesh mongar <yogesh.mongar@selise.ch>
2021-10-30 10:32:15 +0600

    sidekiq statistics

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,3 +7,7 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { namespace: ENV['REDIS_NAMESPACE'], url: ENV['REDIS_URL'] }
 end
+
+Sidekiq::Statistic.configure do |config|
+  config.max_timelist_length = 250_000
+end
