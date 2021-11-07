0bcb57b6aaea0aa56912dee7008fccf201e3cf8a
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2021-11-07 11:22:32 +0600

    minor changes

--- clock.rb
+++ clock.rb
@@ -4,15 +4,11 @@ require 'clockwork'
 require 'active_support/time'
 
 module Clockwork
-  handler do |job|
-    puts "Running #{job}"
-  end
-
-  every(1.minute, 'Before due Date') do
+  every(1.day, 'Before due Date', at: '09:00') do
     `rake reminder:due_date_tomorrow`
   end
 
-  every(2.minutes, 'on due Date') do
+  every(1.day, 'on due Date', at: '17:00') do
     `rake reminder:due_date_today`
   end
 end
