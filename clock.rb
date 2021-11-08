cf89de91d0de0c1165489f62ec6e9a19d7afb079
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2021-11-08 12:58:58 +0600

    created servicefile

--- clock.rb
+++ clock.rb
@@ -3,14 +3,13 @@
 require './config/boot'
 require './config/environment'
 require 'clockwork'
-Rails.application.load_tasks
 
 module Clockwork
   every(1.minute, 'Before due Date') do
-    Rake::Task['reminder:due_date_tomorrow'].invoke
+    ::Projects::Reminders::DueDateTomorrow.call
   end
 
   every(1.minute, 'on due Date') do
-    Rake::Task['reminder:due_date_today'].invoke
+    ::Projects::Reminders::DueDateToday.call
   end
 end
