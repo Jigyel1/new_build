c5ea15ede76d0e746604e09b3b97cf9af59e36b8
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2021-11-08 12:16:33 +0600

    minor changes

--- clock.rb
+++ clock.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
+require './config/boot'
+require './config/environment'
 require 'clockwork'
-require 'active_support/time'
 
 module Clockwork
   every(1.minute, 'Before due Date') do
     `rake reminder:due_date_tomorrow`
   end
 
-  every(2.minutes, 'on due Date') do
+  every(1.minute, 'on due Date') do
     `rake reminder:due_date_today`
   end
 end
