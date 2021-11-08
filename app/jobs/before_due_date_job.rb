efa953b219afaad5ea388974fbc4cc853fbfae65
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2021-11-08 13:43:16 +0600

    minor changes

--- app/jobs/before_due_date_job.rb
+++ app/jobs/before_due_date_job.rb
@@ -4,8 +4,7 @@ class BeforeDueDateJob < ApplicationJob
   queue_as :before_due_date
 
   def perform
-    binding.pry
-    Projects::Task.connection.where(due_date: Date.tomorrow).group_by(&:assignee_id).each do |assignee_id, tasks|
+    Projects::Task.where(due_date: Date.tomorrow).group_by(&:assignee_id).each do |assignee_id, tasks|
       TaskMailer.notify_before_due_date(assignee_id, tasks).deliver
     end
   end
--- app/jobs/on_due_date_job.rb
+++ app/jobs/on_due_date_job.rb
@@ -4,7 +4,7 @@ class OnDueDateJob < ApplicationJob
   queue_as :on_due_date
 
   def perform
-    Projects::Task.connection.where(due_date: Date.current).group_by(&:assignee_id).each do |assignee_id, tasks|
+    Projects::Task.where(due_date: Date.current).group_by(&:assignee_id).each do |assignee_id, tasks|
       TaskMailer.notify_on_due_date(assignee_id, tasks).deliver
     end
   end
