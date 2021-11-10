b90e558c9067eddf6216424f779bcbbaa080568d
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2021-11-10 13:00:51 +0600

    mailer email change

---
+++ app/graphql/mutations/projects/export_building.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Projects
+    class ExportBuilding < BaseMutation
+      argument :id, ID, required: true
+      field :url, String, null: true
+
+      # from the ID, create excel/csv, then send url of that file to FE.
+      def resolve(id:)
+        { url: ::Projects::BuildingExporter.new(current_user: current_user, id: id).call }
+      end
+    end
+  end
+end
+
