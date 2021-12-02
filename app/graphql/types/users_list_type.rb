68fc2699c4c87490c89ff1827c95b6ee358e4156
jigyel dorji <jigyel.dorji@selise.ch>
2021-12-02 13:17:43 +0600

    remove permission field from userlist

--- app/graphql/types/users_list_type.rb
+++ app/graphql/types/users_list_type.rb
@@ -7,13 +7,8 @@ module Types
     field :email, String, null: true
     field :active, Boolean, null: true
     field :role, String, null: true
-    field :permissions, [Types::PermissionType], null: true
     field :phone, String, null: true
     field :department, String, null: true
     field :avatar_url, String, null: true
-
-    def permissions
-      User.find(object.id).permissions
-    end
   end
 end
