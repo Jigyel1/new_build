2294bd84d7478c1973d59cbcc9e088a6f96b4c9b
jigyel dorji <jigyel.dorji@selise.ch>
2021-12-15 12:20:20 +0600

    specs for offer models

--- spec/models/admin_toolkit/offer_marketing_spec.rb
+++ spec/models/admin_toolkit/offer_marketing_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe AdminToolkit::OfferMarketing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:activity_name) }
+    it { is_expected.to validate_presence_of(:value) }
+  end
 end
