ee2dd88a09ffdc8e9459f166bf9d6a5b7afd7417
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-01-03 17:55:51 +0600

    minor changes

--- spec/models/admin_toolkit/cost_threshold_spec.rb
+++ spec/models/admin_toolkit/cost_threshold_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe AdminToolkit::CostThreshold, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:not_exceeding) }
     it { is_expected.to validate_presence_of(:exceeding) }
-
     it { is_expected.to validate_numericality_of(:not_exceeding).is_greater_than_or_equal_to(0) }
   end
 end
