7a0122d0eec7cfb40bdb87dfc98db2c05c1285a4
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-09 18:38:24 +0600

    revert

--- app/services/projects/pct_finder.rb
+++ app/services/projects/pct_finder.rb
@@ -2,7 +2,7 @@
 
 module Projects
   class PctFinder < BaseService
-    attr_accessor :type, :id
+    attr_accessor :project, :type
 
     include PctHelper
 
@@ -13,27 +13,23 @@ module Projects
     private
 
     def pct_value # rubocop:disable Metrics/AbcSize
-      if project.standard?
-        admin_toolkit_pct(hfc_pct_cost.payback_period, hfc_pct_cost.build_cost) if project.standard?
-      else
-        specify_pct_value if cost_type.valid_combination?
-        admin_toolkit_pct(ftth_pct_cost.payback_period, ftth_pct_cost.build_cost) if hfc_too_expensive
-        admin_toolkit_pct(hfc_pct_cost.payback_period, hfc_pct_cost.build_cost) if ftth_too_expensive
+      unless project.standard?
+        return specify_pct_value if cost_type.valid_combination?
+        return admin_toolkit_pct(ftth_pct_cost.payback_period, ftth_pct_cost.build_cost) if hfc_too_expensive
+        return admin_toolkit_pct(hfc_pct_cost.payback_period, hfc_pct_cost.build_cost) if ftth_too_expensive
       end
+
+      admin_toolkit_pct((hfc_pct_cost.payback_period / 12), hfc_pct_cost.build_cost) if project.standard?
     end
 
     def pct_cost
-      if project.standard?
-        hfc_pct_cost if project.standard?
-      else
-        specify_pct_cost if cost_type.valid_combination?
-        ftth_pct_cost if hfc_too_expensive
-        hfc_pct_cost if ftth_too_expensive
+      unless project.standard?
+        return specify_pct_cost if cost_type.valid_combination?
+        return ftth_pct_cost if hfc_too_expensive
+        return hfc_pct_cost if ftth_too_expensive
       end
-    end
 
-    def project
-      @_project ||= Project.find(id)
+      hfc_pct_cost if project.standard?
     end
   end
 end
