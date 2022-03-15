020200d6b324c652a05225cc388d7c651851384b
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-15 18:26:38 +0600

    refractor

--- db/migrate/20220306212410_update_projects_pct_costs.rb
+++ db/migrate/20220306212410_update_projects_pct_costs.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class UpdateProjectsPctCosts < ActiveRecord::Migration[6.1]
-  safety_assured do
-    change_column :projects_pct_costs, :payback_period, :float
-  end
+  safety_assured { change_column :projects_pct_costs, :payback_period, :float }
 end
