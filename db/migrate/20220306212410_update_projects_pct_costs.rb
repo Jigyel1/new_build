f114087b337c57512b33ef8fdc28914f4f72baaf
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-16 21:36:50 +0600

    strong migration fix

--- db/migrate/20220306212410_update_projects_pct_costs.rb
+++ db/migrate/20220306212410_update_projects_pct_costs.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class UpdateProjectsPctCosts < ActiveRecord::Migration[6.1]
-  safety_assured { change_column :projects_pct_costs, :payback_period, :float }
+  def up
+    safety_assured { change_column :projects_pct_costs, :payback_period, :float }
+  end
+
+  def down
+    remove_columns :projects_pct_costs, :payback_period
+  end
 end
