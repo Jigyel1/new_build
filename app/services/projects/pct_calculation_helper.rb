3a84490163c570fff427f0757cd18b8eab6a16ea
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-17 16:45:51 +0600

    pct fix

--- app/services/projects/pct_calculation_helper.rb
+++ app/services/projects/pct_calculation_helper.rb
@@ -7,7 +7,7 @@ module Projects
     end
 
     def pct_cost
-      @_pct_cost ||= connection_cost.build_pct_cost || connection_cost.pct_cost.presence
+      @_pct_cost ||= connection_cost.pct_cost.presence || connection_cost.build_pct_cost
     end
 
     private
