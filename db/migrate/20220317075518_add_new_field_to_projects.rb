212717b734c0b3939a4a30402660cefdd84dab2e
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-17 14:47:04 +0600

    migration

--- db/migrate/20220317075518_add_new_field_to_projects.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddNewFieldToProjects < ActiveRecord::Migration[6.1]
-  def change
-    add_column :projects, :confirmation_status, :string, default: 'New'
-  end
-end
