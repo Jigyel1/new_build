0aba2f4280ba11149804eb1a69e577faa8156e92
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-17 23:07:58 +0600

    penetration fix

--- etl/penetrations/transform.rb
+++ etl/penetrations/transform.rb
@@ -18,9 +18,9 @@ module Penetrations
       penetration = AdminToolkit::Penetration.find_or_initialize_by(zip: row[PenetrationsImporter::ZIP])
       competition = ::AdminToolkit::Competition.find_by(name: row.delete_at(COMPETITION))
 
-      penetration.update(HEADERS.zip(row).to_h)
+      penetration.assign_attributes(HEADERS.zip(row).to_h)
 
-      penetration.kam_region = ::AdminToolkit::KamRegion.find_by!(name: row[KAM_REGION])
+      penetration.kam_region = ::AdminToolkit::KamRegion.find_by(name: row[KAM_REGION])
       penetration.penetration_competitions.find_or_initialize_by(competition: competition)
       penetration
     rescue ActiveRecord::RecordNotFound => e
