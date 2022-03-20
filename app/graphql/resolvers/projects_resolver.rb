7d3776d3e9bbca2f09619a0c9f9f013af7ab1c42
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-20 21:41:38 +0600

    penetration update

--- app/graphql/resolvers/projects_resolver.rb
+++ app/graphql/resolvers/projects_resolver.rb
@@ -17,6 +17,7 @@ module Resolvers
     option(:cities, type: [String]) { |scope, value| scope.where(city: value) }
     option(:zips, type: [String]) { |scope, value| scope.where(zip: value) }
     option(:kam_regions, type: [String]) { |scope, value| scope.where(kam_region: value) }
+    option(:label_list, type: [String]) { |scope, value| scope.where(label_list: value) }
 
     option :buildings_count, type: [Int], with: :apply_buildings_filter, description: <<~DESC
       Send min and max values in the array. eg. [2, 10]. Only the first two values will be picked.
