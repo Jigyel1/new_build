a3ca7b024e3df6697f6332178d75fdaa4096a63a
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-20 09:54:35 +0600

    pdf fix

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -50,4 +50,16 @@ class ProjectDecorator < ApplicationDecorator
   def priority_check
     project.priority_tac.presence || project.priority
   end
+
+  def building_type_formatter
+    if  project.efh? || project.defh? || project.mfh?
+      project.building_type.try(:upcase)
+    elsif project.refh?
+      I18n.t("pdf.project.refh")
+    elsif project.others?
+      "#{project.building_type.try(:titleize)} - #{project.description_on_other.try(:titleize)}"
+    else
+      project.building_type.try(:titleize)
+    end
+  end
 end
