5556a117f03516567c97fc97635991fa9beb0af3
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-20 08:43:49 +0600

    task email and payback period format

--- app/graphql/mutations/projects/update_payback_period.rb
+++ app/graphql/mutations/projects/update_payback_period.rb
@@ -5,7 +5,7 @@ module Mutations
     class UpdatePaybackPeriod < BaseMutation
       class UpdatePaybackPeriodAttributes < Types::BaseInputObject
         argument :connection_cost_id, ID, required: true
-        argument :months, Int, required: false
+        argument :months, Float, required: false
       end
 
       argument :attributes, UpdatePaybackPeriodAttributes, required: true
