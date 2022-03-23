edef9f3b65a9ca294bbfdafcece6c61749c327e5
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-23 17:26:20 +0600

    added view

--- app/services/projects/project_label_helper.rb
+++ app/services/projects/project_label_helper.rb
@@ -5,8 +5,10 @@ module Projects
     private
 
     def added_project_label
-      labels = Projects::LabelGroup.where(system_generated: false).find_by(project_id: project.id).label_list
-      project.update!(added_labels: labels)
+      return unless Projects::LabelGroup.where(system_generated: false).present?
+      
+      labels = Projects::LabelGroup.where(system_generated: false).find_by(project_id: project.id)
+      project.update!(added_labels: labels.label_list)
     end
   end
 end
