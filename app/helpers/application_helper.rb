059d4e3e27526bfb9c9b5c657de809bb62b18c3c
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-24 20:38:17 +0600

    refractor

--- app/helpers/application_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module ApplicationHelper
-  def render_if_exists(path_to_partial, args = {})
-    render(partial: path_to_partial, **args) if lookup_context.find_all(path_to_partial, [], true).any?
-  end
-end
