741439c0b177ef894357f4f47e60afb24df8df8b
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-24 20:34:03 +0600

    refractor

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -62,8 +62,4 @@ class ProjectDecorator < ApplicationDecorator
   def url_present?
     project.gis_url.present? && project.info_manager_url.present?
   end
-
-  def render_if_exists(path_to_partial, args = {})
-    render(partial: path_to_partial, **args) if lookup_context.find_all(path_to_partial, [], true).any?
-  end
 end
