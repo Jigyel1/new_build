933162c0cf1e06eaba48bd6c298762025420db08
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-29 19:43:48 +0600

    migration

---
+++ db/migrate/20220328093946_update_projects_tasks.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UpdateProjectsTasks < ActiveRecord::Migration[6.1]
+  def change
+    safety_assured do
+      change_table :projects_tasks, bulk: true do |t|
+        t.string :project_id, null: true
+        t.string :building_id, null: true
+        t.string :project_name, null: true
+      end
+    end
+  end
+end
