fbe6b61a49ff71580345c63a6879dbe344624cfa
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-30 01:39:42 +0600

    rubocop fix

--- config/routes.rb
+++ config/routes.rb
@@ -43,8 +43,8 @@ Rails.application.routes.draw do
     )
 
     as :user do
-      post "users/sign_in" => "devise/sessions#create", :as => :user_session
-      delete "users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+      post 'users/sign_in' => 'devise/sessions#create', :as => :user_session
+      delete 'users/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
     end
   end
 
