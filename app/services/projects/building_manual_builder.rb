736131a8a16321a0bd45b91c8082937040a21e98
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-30 14:52:51 +0600

    code refractor

--- app/services/projects/building_manual_builder.rb
+++ app/services/projects/building_manual_builder.rb
@@ -11,16 +11,16 @@ module Projects
     private
 
     def create_building
-      1.upto(buildings_count) do |index|
-        site_address = project.site_address[index - 1]
-
-        project.buildings.build(
-          name: site_address['name'],
-          assignee: project.assignee,
-          apartments_count: site_address['apartments_count'],
-          move_in_starts_on: site_address['move_in_starts_on'],
-          address_attributes: site_address['address']
-        )
+      1.upto(buildings_count) do |_index|
+        project.site_address.each do |site_address|
+          project.buildings.build(
+            name: site_address['name'],
+            assignee: project.assignee,
+            apartments_count: site_address['apartments_count'],
+            move_in_starts_on: site_address['move_in_starts_on'],
+            address_attributes: site_address['address']
+          )
+        end
       end
     end
   end
