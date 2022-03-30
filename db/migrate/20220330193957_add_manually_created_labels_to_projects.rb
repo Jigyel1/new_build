962cc30f00915c200bd894b57c20e1bf4188b557
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-31 03:51:16 +0600

    migrations

--- db/migrate/20220330193957_add_manually_created_labels_to_projects.rb
+++ db/migrate/20220330193957_add_manually_created_labels_to_projects.rb
@@ -1,5 +1,11 @@
 class AddManuallyCreatedLabelsToProjects < ActiveRecord::Migration[6.1]
   def change
-    add_column :projects, :manually_created_labels, :string
+    def change
+      safety_assured do
+        change_table :projects, bulk: true do |t|
+          t.text :manually_created_labels, array: true, default: []
+        end
+      end
+    end
   end
 end
