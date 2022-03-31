e5ae0b159aedf25b6c65c8a1e36d6fc23b54adfb
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-03-31 22:16:12 +0600

    Plz Bug fix

--- etl/penetrations/transform.rb
+++ etl/penetrations/transform.rb
@@ -32,7 +32,7 @@ module Penetrations
 
     def build_competition(penetration, competitions)
       competitions.each do |name|
-        competition = ::AdminToolkit::Competition.find_by(name: name.split)
+        competition = ::AdminToolkit::Competition.find_by(name: name.strip)
         penetration.penetration_competitions.find_or_initialize_by(competition: competition)
       end
     end
