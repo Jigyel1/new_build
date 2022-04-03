b8f4bc912fff72f1fb20508fce012971a949908e
Jigyel Yeshi Dorji <jigyel.dorji@selise.ch>
2022-04-03 18:27:57 +0600

    set default New

--- db/migrate/20220318172640_add_new_field_to_projects.rb
+++ db/migrate/20220318172640_add_new_field_to_projects.rb
@@ -5,7 +5,7 @@ class AddNewFieldToProjects < ActiveRecord::Migration[6.1]
     safety_assured do
       change_table :projects, bulk: true do |t|
         t.references :kam_assignee, foreign_key: { to_table: :telco_uam_users }, type: :uuid
-        t.string :confirmation_status, null: true
+        t.string :confirmation_status, null: true, default: 'New'
         t.string :description_on_other, null: true
       end
     end
