e156855cbcb2d1ca69dccd826b22680c9ed55e6e
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-04-14 16:12:26 +0600

    extract enum to projects_list.rb

--- app/models/concerns/enumable/project.rb
+++ app/models/concerns/enumable/project.rb
@@ -11,15 +11,6 @@ module Enumable
       enum access_technology: { ftth: 'FTTH', hfc: 'HFC', third_party: 'Third Party' }
       enum access_technology_tac: { ftth: 'FTTH', hfc: 'HFC', third_party: 'Third Party' }, _suffix: :tac
 
-      enum confirmation_status: {
-        new_offer: 'New',
-        negotiation: 'Negotiation',
-        offered: 'Offered',
-        signed: 'Signed',
-        internal_clarification: 'Internal Clarification',
-        customer_not_interested: 'Customer Not Interested'
-      }
-
       enum building_type: {
         efh: 'EFH',
         defh: 'DEFH',
