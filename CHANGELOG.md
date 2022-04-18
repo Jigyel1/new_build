8db7f8e02ca3348e6a270b7921e9eac28ee98462
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-04-19 01:38:02 +0600

    trackable_id filter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/).
 
+## [1.7.0] - 2022-04-26
+
+### Features
+- Trackable id filter added to activities resolver.
+
 ## [1.6.2] - 2022-04-12
 
 ### Fixes
--- spec/graphql/resolvers/activities_resolver_spec.rb
+++ spec/graphql/resolvers/activities_resolver_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe Resolvers::ActivitiesResolver do
   let_it_be(:management) { create(:user, :management) }
   let_it_be(:kam) { create(:user, :kam) }
   let_it_be(:kam_a) { create(:user, :kam) }
+  let_it_be(:project) { create(:project, assignee: kam, kam_assignee: kam, project_nr: '2222') }
+  let_it_be(:building) { create(:building, project: project, external_id: '112233') }
   before_all { create_activities }
 
   describe '.resolve' do
@@ -398,6 +400,38 @@ RSpec.describe Resolvers::ActivitiesResolver do
                 recipient_email: management.email, status_text: t('activities.deactivated'))
             ]
           )
+        end
+      end
+
+      context 'with activities filter with trackable_id' do
+        before do
+          create(
+            :activity,
+            owner: super_user,
+            trackable: project,
+            action: :project_created,
+            log_data: {
+              owner_email: super_user.email,
+              parameters: { entry_type: project.entry_type, project_name: project.name }
+            }
+          )
+        end
+
+        let!(:param) { { trackable_id: project.id, name: project.name } }
+
+        it 'returns activities with matching trackable id' do
+          activities, errors = paginated_collection(
+            :activities,
+            query(trackable_id: param[:trackable_id], query: param[:name]),
+            current_user: super_user
+          )
+          expect(activities.pluck('displayText')).to eq(
+            [
+             t('activities.project.project_created.owner',
+               project_name: project.name)
+            ]
+          )
+          expect(errors).to be_nil
         end
       end
     end
