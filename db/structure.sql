5c8af1bf8b6e59f2c0be66e2a0325683834f652e
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-04-22 11:59:23 +0600

    structure update

--- db/structure.sql
+++ db/structure.sql
@@ -716,8 +716,7 @@ CREATE TABLE public.admin_toolkit_penetrations (
     hfc_footprint boolean NOT NULL,
     type character varying NOT NULL,
     created_at timestamp(6) without time zone NOT NULL,
-    updated_at timestamp(6) without time zone NOT NULL,
-    strategic_partner character varying
+    updated_at timestamp(6) without time zone NOT NULL
 );
 
 
@@ -2328,7 +2327,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20220330200607'),
 ('20220411192300'),
 ('20220413101543'),
-('20220413234310'),
-('20220421174202');
+('20220413234310');
 
 
