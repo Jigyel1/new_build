4d0dacf7341f24909a067384a46b08f6ddf52192
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-04-28 16:40:54 +0600

    rubocop fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.cache_store = :redis_cache_store, { url: ENV['REDIS_HOST'] }
+  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_HOST', nil) }
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
