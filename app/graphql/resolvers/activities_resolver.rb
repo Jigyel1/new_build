928f67cb481e3345eb8f4f63b539074b9405f719
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-05-03 18:52:49 +0600

    activities filter

--- app/graphql/resolvers/activities_resolver.rb
+++ app/graphql/resolvers/activities_resolver.rb
@@ -15,7 +15,7 @@ module Resolvers
       be returned. Else the logs between the first two dates will be returned. Date format expected -> '2021-05-06'
     DESC
 
-    option(:trackable_id, type: String) { |scope, value| scope.where(trackable_type: 'Project', trackable_id: value) }
+    option :trackable_id, type: String, with: :apply_trackable_id_filter
     option :query, type: String, with: :apply_search
   end
 end
