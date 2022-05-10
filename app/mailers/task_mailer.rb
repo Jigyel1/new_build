a71f51ca8cb10fe0560f72f982977c180bc26807
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-05-10 10:48:03 +0600

    revert

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -2,20 +2,20 @@
 
 class TaskMailer < ApplicationMailer
   def notify_created(user_id, task_id, project_id)
-    notify(user_id, nil, task_id, project_id, :task_created)
+    notify(user_id, :task_created, task_id, project_id)
   end
 
   def notify_before_due_date(user_id, tasks)
-    notify(user_id, tasks, nil, nil,:notify_before_due_date)
+    notify(user_id, :notify_before_due_date, tasks)
   end
 
   def notify_on_due_date(user_id, tasks)
-    notify(user_id, tasks, nil, nil,:notify_on_due_date)
+    notify(user_id, :notify_on_due_date, tasks)
   end
 
   private
 
-  def notify(user_id, tasks, task_id, project_id, subject)
+  def notify(user_id, subject, tasks = nil, task_id = nil, project_id = nil)
     @user = User.find(user_id)
     @tasks = tasks
     @task = Projects::Task.find(task_id) if task_id.present?
