3541aebab1428e376bbed64e508630554a1a7fc4
jigyel yeshi dorji <jigyel.dorji@selise.ch>
2022-05-13 13:02:26 +0600

    csv exports

--- spec/graphql/mutations/export_activities_spec.rb
+++ spec/graphql/mutations/export_activities_spec.rb
@@ -11,21 +11,51 @@ RSpec.describe Mutations::ExportActivities do
   let_it_be(:management) { create(:user, :management) }
   let_it_be(:kam) { create(:user, :kam) }
   let_it_be(:kam_a) { create(:user, :kam) }
+  let_it_be(:project) { create(:project, assignee: kam, kam_assignee: kam, project_nr: '2222') }
+  let_it_be(:building) { create(:building, project: project, external_id: '112233') }
   before_all { create_activities }
 
   describe '.resolve' do
-    it 'exports activities to csv' do
-      response, errors = formatted_response(query, current_user: super_user, key: :exportActivities)
-      expect(errors).to be_nil
-      expect { (uri = URI.parse(response.url)) && uri.host }.not_to raise_error
+    context 'when send without any arguments' do
+      it 'exports all activities to csv' do
+        response, errors = formatted_response(query, current_user: super_user, key: :exportActivities)
+        expect(errors).to be_nil
+        expect { (uri = URI.parse(response.url)) && uri.host }.not_to raise_error
+      end
+    end
+
+    context 'when trackable id and user id is provided in arguments' do
+      before do
+        create(
+          :activity,
+          owner: super_user,
+          trackable: project,
+          action: :project_created,
+          log_data: {
+            owner_email: super_user.email,
+            parameters: { entry_type: project.entry_type, project_name: project.name }
+          }
+        )
+      end
+
+      let(:param) {{ trackable_id: project.id, user_id: super_user.id }}
+
+      it 'exports projects specific to trackable id and user id' do
+        response, errors = formatted_response(query(param), current_user: super_user, key: :exportActivities)
+        expect(errors).to be_nil
+        expect { (uri = URI.parse(response.url)) && uri.host }.not_to raise_error
+      end
     end
   end
 
-  def query(_args = {})
+  def query(args = {})
+    user_id = args[:user_id] || ''
+    trackable_id = args[:trackable_id] || ''
+
     <<~GQL
       mutation {
         exportActivities(
-        input: { attributes: {}}) { url }
+        input: { attributes: {trackableId: "#{user_id}" userIds: ["#{trackable_id}"]}}) { url }
       }
     GQL
   end
